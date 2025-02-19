dd5bd73b16068a78489f7e80c3c5db95ab1eff0c
Abdullah Khan <amkhan8466@gmail.com>
2025-02-19 23:32:30 +0500

    First commit

---
+++ Home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+
+    persons =[ 
+        {'Name':'Abdullah','age':21},
+        {'Name':'ali','age':22},
+        {'Name':'usman','age':20},
+        {'Name':'Abdulrehman','age':17}
+    ]
+    return render(request,"index.html" ,context={'persons': persons})
+def about(request):
+    return render(request,"about.html")
+def contact(request):
+    return render(request,"contact.html")
