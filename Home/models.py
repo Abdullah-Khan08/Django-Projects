eb7b8dde1ce77adb5228ee2ef2e8707d4bdb3134
Abdullah Khan <amkhan8466@gmail.com>
2025-02-27 22:31:20 +0500

    First commit

--- Home/models.py
+++ Home/models.py
@@ -9,4 +9,11 @@ class student(models.Model):
     Dob=models.DateField(null=True ,blank=True)
     email=models.EmailField(null=True ,blank=True)
     address=models.TextField(null=True ,blank=True)
-    image=models.ImageField(null=True ,blank=True)    
+    image=models.ImageField(null=True ,blank=True)  
+class cars(models.Model):
+    car_name= models.CharField(max_length=100)
+    speed=models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return f"{self.car_name} - {self.speed} km/h"
+    
